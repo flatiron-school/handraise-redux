fae16b0ba2798153de54d798ea09b15f581295b4
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-20 15:55:34 -0400

    working on authorization. authentication complete

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, :presence => { :on => :create }
 
+  USER_ROLES = {
+    0 => :admin,
+    10 => :student
+  }
+
+  def self.user_roles
+    USER_ROLES
+  end
+
   def self.authenticate(email, password)
     user = User.find_by_email(email)
     if user.authenticate(password)
@@ -21,8 +30,18 @@ class User < ActiveRecord::Base
     end    
   end
 
-  def logged_in?
-    true if self.id == session[:user_id]
+  def can_edit?(issue)
+    true if (self.role == 0 || issue.user_id == self.id)
   end
+
+  def can_destroy?(issue)
+    true if (self.role == 0 || issue.user_id == self.id)
+  end
+
+  # def can?(issue, action)
+  #   true if (self.role == 0 || issue.user_id == self.id)
+  # end
+
+  # current_user.can?(edit, object)
 
 end
