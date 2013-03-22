d4e4eb95928a0a9711d1b5a2bcb6793624511083
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-22 11:49:18 -0400

    added ability to link to gist, cleaned up new and show issue views

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320150832) do
+ActiveRecord::Schema.define(:version => 20130322150420) do
 
   create_table "issues", :force => true do |t|
     t.text     "content"
     t.string   "title"
     t.integer  "user_id"
     t.integer  "status"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
     t.integer  "assignee_id"
+    t.string   "relevant_gist"
   end
 
   create_table "users", :force => true do |t|
