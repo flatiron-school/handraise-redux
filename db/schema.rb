b304be298ce786037bb9d5720240c601699f3124
janeeats <jane.vora@gmail.com>
2013-03-27 11:30:39 -0400

    install twilio gem and now users receive sms text when their issue is assigned

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326160709) do
+ActiveRecord::Schema.define(:version => 20130327152508) do
 
   create_table "issues", :force => true do |t|
     t.text     "content"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130326160709) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "profile_url"
+    t.string   "cell"
   end
 
   create_table "votes", :force => true do |t|
