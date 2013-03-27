bd37d54de39f5285330ce0b6a0bddace1915f277
janeeats <jane.vora@gmail.com>
2013-03-27 14:33:15 -0400

    Modify some views

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -105,46 +105,6 @@ class Issue < ActiveRecord::Base
     Issue.not_closed.where("assignee_id IS NULL")
   end
 
-  def self.wait_time_total
-    (Issue.wait_time_open_in_seconds + Issue.wait_time_closed_in_seconds)/60
-  end
-
-  def self.average_wait_time_total
-    Issue.wait_time_total/(Issue.total_open_issues + Issue.total_closed_issues)
-  end
-
-  def self.wait_time_open_in_seconds
-    Issue.not_closed.inject(0) do |time, issue|
-      time + (Time.now - issue.created_at) 
-    end
-  end
-
-  def self.wait_time_closed_in_seconds
-    Issue.closed.inject(0) do |time, issue|
-      time + (issue.updated_at - issue.created_at) 
-    end
-  end
-
-  def self.total_open_issues
-    Issue.not_closed.size
-  end
-
-  def self.total_closed_issues
-    Issue.closed.size
-  end
-
-  def self.average_wait_time_open
-    if total_open_issues == 0
-      0
-    else
-      (Issue.wait_time_open_in_seconds/60)/Issue.total_open_issues
-    end
-  end
-
-  def self.average_wait_time_closed
-    (Issue.wait_time_closed_in_seconds/60)/Issue.total_closed_issues
-  end
-
   def answered?
     true unless self.responses.where(:answer => 1).empty?
   end
