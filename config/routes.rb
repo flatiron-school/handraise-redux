ea7bcbe7a76d337ea4613f6a3694f872bf7f4e4f
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-04-03 17:29:01 -0400

    add helped and unhelp in issue controller

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,8 @@ App::Application.routes.draw do
 
   get 'assigned' => 'issues#assigned'
 
-  get 'helped/:id' => 'issues#helped', :as => "helped"
-  get 'unhelped/:id' => 'issues#unhelped', :as => "unhelped"
+  get 'issues/:id/helped' => 'issues#helped', :as => "helped"
+  get 'issues/:id/unhelp' => 'issues#unhelp', :as => "unhelp"
 
   get 'users/:id/admin' => 'users#admin', :as => "makeadmin"
   get 'users/:id/notadmin' => 'users#notadmin', :as => "removeadmin"
