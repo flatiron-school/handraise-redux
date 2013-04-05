461dbbd9a190ce48f00ef0eea09eb74ec27b17d4
ei-lene <eileneheng@gmail.com>
2013-04-05 11:56:16 -0400

    CSS edits and some flow fixes

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,3 +2,4 @@ Rails.application.config.middleware.use ExceptionNotifier,
   :email_prefix => "Handrai.se Error: ",
   :sender_address => %{"notifier" <postmaster@handraise.mailgun.org>},
   :exception_recipients => %w{ei-lene.heng@flatironschool.com anthony.wijnen@flatironschool.com jane.vora@flatironschool.com eugene.wang@flatironschool.com}
+
