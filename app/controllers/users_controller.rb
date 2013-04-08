fc2f0f64683cbd95502eed106564bb65cb9fa4ac
ei-lene <eileneheng@gmail.com>
2013-04-08 11:44:07 -0400

    Redirect user after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to issues_path, notice: 'User was successfully created and logged in' }
+        format.html { redirect_to user_path(@user), notice: 'User was successfully created and logged in' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
