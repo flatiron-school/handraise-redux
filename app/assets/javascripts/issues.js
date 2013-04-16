79ffb936cfb302bbc0036b8f3d8730e4faf0b539
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-16 12:30:45 -0400

    fixed errors in js

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -52,30 +52,8 @@ $(function(){
     }
   })
 
-// (function($){
-//   $.fn.extend({
-//     limiter: function(limit, elem) {
-//       $(this).on("keyup focus", function() {
-//         setCount(this, elem);
-//       });
-//       function setCount(src, elem) {
-//         var chars = src.value.length;
-//         if (chars > limit) {
-//             src.value = src.value.substr(0, limit);
-//             chars = limit;
-//         }
-//         elem.html( limit - chars );
-//       }
-//       setCount($(this)[0], elem);
-//     }
-//   });
-// });
-
   var elem = $("#chars");
-  // var desc = "Your description is too short. Please give us more details of your issue. Help us, help you!";
 
   $("#issue_content").limiter(80, elem);
-  // $("#issue_content").on("keyup focus", function(){
-  //   $("#issue_content").append('<p>' + desc + '</p>');
-  // })
+
 });
