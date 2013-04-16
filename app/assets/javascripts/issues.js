88eec7fcdd01a1239f6f9a855ee65d4364cba06f
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-16 10:33:52 -0400

    tooltips

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -2,3 +2,17 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
+$(function(){ 
+  $('.edit_button a').hover(function(){
+    $(this).tooltip('show')
+  });
+  $('.resolve_button a').hover(function(){
+    $(this).tooltip('show')
+  });
+  $('.delete_button a').hover(function(){
+    $(this).tooltip('show')
+  });  
+  $('.more_help_button a').hover(function(){
+    $(this).tooltip('show')
+  });
+})
