895e61b39ca3b88d8489e616bfca3cc076a7bd92
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-16 12:26:18 -0400

    added content validation for length

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -15,4 +15,67 @@ $(function(){
   $('.more_help_button a').hover(function(){
     $(this).tooltip('show')
   });
-})
+
+  $.fn.extend({
+    limiter: function(limit, elem){
+      console.log(this);
+      $(this).on("keyup focus", function() {
+        setCount(this, elem);
+      });
+      function setCount(src, elem) {
+        var chars = src.value.length;
+        var desc = "Your description is too short. Please give us more details of your issue. Help us, help you!";
+        var first = $('.length_notice:first');
+        if(chars == 0){
+          first.text("Your description is too short. Please give us more details of your issue. Help us, help you!");
+          first.css("color", "red");
+        } else if((chars >= 1) && (chars < limit * 0.25)){
+          first.text("That's it? We need more information.");
+          first.css("color", "red");
+        } else if((chars >= limit * 0.25) && (chars < limit * 0.75)){
+          first.text("Still not quite cutting it.");
+          first.css("color", "orange");
+        } else if((chars >= limit * 0.75) && (chars < limit)){
+          first.text("You're almost there!");
+          first.css("color", "purple");
+        }
+        if (chars > limit) {
+            // src.value = src.value.substr(0, limit);
+            // chars = limit;
+            $('.length_notice').hide();
+        } else {
+          $('.length_notice').show();
+        }
+        elem.html( limit - chars );
+      }
+      setCount($(this)[0], elem);
+    }
+  })
+
+// (function($){
+//   $.fn.extend({
+//     limiter: function(limit, elem) {
+//       $(this).on("keyup focus", function() {
+//         setCount(this, elem);
+//       });
+//       function setCount(src, elem) {
+//         var chars = src.value.length;
+//         if (chars > limit) {
+//             src.value = src.value.substr(0, limit);
+//             chars = limit;
+//         }
+//         elem.html( limit - chars );
+//       }
+//       setCount($(this)[0], elem);
+//     }
+//   });
+// });
+
+  var elem = $("#chars");
+  // var desc = "Your description is too short. Please give us more details of your issue. Help us, help you!";
+
+  $("#issue_content").limiter(80, elem);
+  // $("#issue_content").on("keyup focus", function(){
+  //   $("#issue_content").append('<p>' + desc + '</p>');
+  // })
+});
